a9db593e66d9e6c6d1f49208a68f66df4c8e74a8
Tarun0204 <127747782+Tarun0204@users.noreply.github.com>
2024-04-29 18:36:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerText=localStorage.getItem('user-name');
+document.getElementById("email").innerText=localStorage.getItem('user-email');
+document.getElementById("number").innerText=localStorage.getItem('user-number');
